77cd9deae03ecdb4edc24ffb080b0ea57e9b0613
Matthieu Fournet <fournet.matthieu@gmail.com>
2012-08-21 20:34:52 +0200

    Add rule (as RegExp) to validate args

--- check_web.py
+++ check_web.py
@@ -43,6 +43,7 @@ class nagiosPlugin(object):
         import argparse
         self._argParser = argparse.ArgumentParser(description = 'Check a web page') # TODO : this is not GENERIC !
         self._argList   = []
+        self._argDict   = {}
 
 
     def addArg(self, argData):
@@ -50,14 +51,18 @@ class nagiosPlugin(object):
         self._argParser.add_argument(
             '-'     + argData['shortOption'],
             '--'    + argData['longOption'],
-            type        = argData['type'],
+#            type        = argData['type'],
+            type        = str,  # Even warn / crit can be strings when defining ranges : "50:100"
             dest        = argData['longOption'],
             required    = argData['required'],
             default     = argData['default'],
             help        = argData['help']
             )
-        self._argList.append(argData['longOption'])
-
+#        self._argList.append(argData['longOption'])
+        self._argDict[argData['longOption']] = {
+            'value' : 0,
+            'rule'  : argData['rule']
+            }
 
     def addArgDebug(self):
         self._argParser.add_argument(
@@ -71,14 +76,35 @@ class nagiosPlugin(object):
 
     def readArgs(self):
         self._args = self._argParser.parse_args()
-        # TODO : check args ?
+        for argName in self._argDict:
+            self._argDict[argName]['value'] = getattr(self._args, argName)
+        self._validateArgs()
+
+
+    def _validateArgs(self):
+        self._args = self._argParser.parse_args()
+#        print self._argDict
+        """
+        for argName in self._argList:
+            argValue = str(getattr(self._args, argName))
+#            argRule = self._argList.str(getattr(self._args, argName))
+            print str(argName) + ' ' + argValue
+        """
 
 
     def showArgs(self):
-        truc = utility()
-        length = truc.lengthOfLongestKey(self._argList)
+        util    = utility()
+#        length  = truc.lengthOfLongestKey(self._argList)
+        length  = util.lengthOfLongestKey(self._argDict)
+        """
         for key in self._argList:
             print str(key).rjust(length + 1) + ': ' + str(getattr(self._args, key))
+        print 'SHOWARGS'
+        print length
+        """
+        for key in self._argDict:
+            print str(key).rjust(length + 1) + ': ' + str(self._argDict[key]['value'])
+#            print 'RULE : ' + self._argDict[key]['rule']
 
 
     def getArgValue(self, argName):
@@ -163,64 +189,71 @@ plugin = check_web()
 plugin.addArg({
         'shortOption'   : 'u',
         'longOption'    : 'url',
-        'type'          : str,
+#        'type'          : str,
         'required'      : True,
         'default'       : None,
-        'help'          : 'URL of page to check with leading "http://"'
+        'help'          : 'URL of page to check with leading "http://"',
+        'rule'          : '^http://.*$'
         })
 
 plugin.addArg({
         'shortOption'   : 'p',
         'longOption'    : 'httpPort',
-        'type'          : str,
+#        'type'          : str,
         'required'      : False,
         'default'       : 80,
         'help'          : 'HTTP port (optional. Defaults to 80)',
+        'rule'          : ''
         })
 
 plugin.addArg({
         'shortOption'   : 'M',
         'longOption'    : 'httpMethod',
-        'type'          : str,
+#        'type'          : str,
         'required'      : False,
         'default'       : 'GET',
         'help'          : 'HTTP method (optional. Defaults to GET)',
+        'rule'          : ''
         })
 
 plugin.addArg({
         'shortOption'   : 'm',
         'longOption'    : 'matchString',
-        'type'          : str,
+#        'type'          : str,
         'required'      : True,
         'default'       : None,
         'help'          : 'String to search on page',
+        'rule'          : ''
         })
 
 plugin.addArg({
         'shortOption'   : 'w',
         'longOption'    : 'warning',
-        'type'          : int,
+#        'type'          : int,
         'required'      : True,
         'default'       : None,
         'help'          : 'warning threshold in ms',
+        'rule'          : ''
         })
 
 plugin.addArg({
         'shortOption'   : 'c',
         'longOption'    : 'critical',
-        'type'          : int,
+#        'type'          : int,
         'required'      : True,
         'default'       : None,
-        'help'          : 'warning threshold in ms',
+        'help'          : 'critical threshold in ms',
+        'rule'          : ''
         })
 
 plugin.addArg({
         'shortOption'   : 'H',
         'longOption'    : 'httpHostHeader',
-        'type'          : str,
+#        'type'          : str,
         'required'      : True,
         'default'       : None,
         'help'          : 'HTTP host header (optional)',
+        'rule'          : ''
         })
 
 
@@ -239,7 +272,7 @@ url = Url({
         'full' : plugin.getArgValue('url')
         })
 
-#debug.die({'exitMessage': 'ARGL !'})
+debug.die({'exitMessage': 'ARGL !'})
 
 
 
