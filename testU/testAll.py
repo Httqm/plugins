8d2e547943c3b75b6f76633b7d8d5aa2aecd417f
Matthieu Fournet <fournet.matthieu@gmail.com>
2012-10-22 22:06:20 +0200

    Run all unit tests with a single command

---
+++ testU/testAll.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Run all unit tests of all "test_*.py" files.
+Source : http://stackoverflow.com/questions/1732438/run-all-unit-test-in-python-directory/1735277#1735277
+"""
+
+import glob
+import unittest
+
+
+test_file_strings   = glob.glob('test_*.py')
+module_strings      = [str[0:len(str)-3] for str in test_file_strings]
+suites              = [unittest.defaultTestLoader.loadTestsFromName(str) for str in module_strings]
+testSuite           = unittest.TestSuite(suites)
+text_runner         = unittest.TextTestRunner().run(testSuite)
