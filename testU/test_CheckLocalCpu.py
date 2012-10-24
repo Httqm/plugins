c3ed5502774a443d6565d5a3faa57a9196650818
Matthieu Fournet <fournet.matthieu@gmail.com>
2012-10-24 22:42:17 +0200

    Add skeleton for unit testing

---
+++ testU/test_CheckLocalCpu.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import unittest
+
+
+########################################## ##########################################################
+# allows importing from parent folder
+# source : http://stackoverflow.com/questions/714063/python-importing-modules-from-parent-folder
+import os
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+os.sys.path.insert(0,parentdir)
+########################################## ##########################################################
+
+
+from modules import CheckLocalCpu
+
+class test_CheckLocalCpu(unittest.TestCase):
+
+    def test1_computeOutput(self):
+        """
+        Given 
+	should return 
+        """
+
+
+        """
+        sleepDurationMicroseconds = 1000	# No matter how long : the return value is a number of µs
+        obj = modules.timer.Timer()
+	obj.start()
+        time.sleep(1.0 * sleepDurationMicroseconds / 1000000)	# expects seconds. "1.0 * ..." is a hack to convert to a float.
+        sleepDuration = obj.stop()
+        self.assertTrue((sleepDuration > sleepDurationMicroseconds) and (sleepDuration < (2 * sleepDurationMicroseconds)))
+        """
+        self.assertTrue(True)
+        
+
+# uncomment this to run this unit test manually
+if __name__ == '__main__':
+    unittest.main()
