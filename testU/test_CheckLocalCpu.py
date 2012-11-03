44026d17e342fe924cd27a8c6c9a92c58dc1eb72
Matthieu Fournet <fournet.matthieu@gmail.com>
2012-11-03 22:51:24 +0100

    Add unit test : making sure we get a WARNING

--- testU/test_CheckLocalCpu.py
+++ testU/test_CheckLocalCpu.py
@@ -18,7 +18,12 @@ from modules import Utility
 from modules import Debug
 
 
+warning     = 75
+critical    = 90
+
+
 class test_CheckLocalCpu(unittest.TestCase):
+
 
     def test1_computeOutput(self):
         """
@@ -33,7 +38,6 @@ class test_CheckLocalCpu(unittest.TestCase):
             objUtility  = self._objUtility
             )
 
-#        self._cpuData = {'softirq': {'cpuPercent': 0.003871639406714247, 'cpuTime': 260.79000000000002}, 'iowait': {'cpuPercent': 0.11486798859431271, 'cpuTime': 7737.3999999999996}, 'system': {'cpuPercent': 1.4258630794508551, 'cpuTime': 96044.800000000003}, 'idle': {'cpuPercent': 94.031635591815515, 'cpuTime': 6333882.7999999998}, 'user': {'cpuPercent': 4.3198738234216778, 'cpuTime': 290982.65000000002}, 'irq': {'cpuPercent': 0.0503180995142802, 'cpuTime': 3389.3800000000001}, 'nice': {'cpuPercent': 0.053569777796624703, 'cpuTime': 3608.4099999999999}}
         myPlugin._cpuData = {
 #            'softirq'   : {'cpuPercent': 1.0,   'cpuTime': 12.12},
 #            'iowait'    : {'cpuPercent': 1.0,   'cpuTime': 12.12},
@@ -42,13 +46,35 @@ class test_CheckLocalCpu(unittest.TestCase):
 #            'user'      : {'cpuPercent': 5.0,   'cpuTime': 12.12},
 #            'irq'       : {'cpuPercent': 1.0,   'cpuTime': 12.12},
 #            'nice'      : {'cpuPercent': 1.0,   'cpuTime': 12.12},
-            'totalWithoutIdle'      : {'cpuPercent': 1.0,   'cpuTime': 12.12}
+
+#            'totalWithoutIdle'      : {'cpuPercent': 76.0,   'cpuTime': 12.12}
+            'totalWithoutIdle': {'cpuPercent': warning - 1, 'cpuTime': 12.34}
             }
 
+        myPlugin.computeOutput(warningThreshold=warning, criticalThreshold=critical)
+        self.assertEqual(myPlugin._exitCode, myPlugin._exitCodes['OK'])
+#        self.assertEqual(True, True)
 
-        myPlugin.computeOutput(warningThreshold=12, criticalThreshold=42)
-#        self.assertEqual(myPlugin._exitCode, myPlugin._exitCodes['OK'])
-        self.assertEqual(True, True)
+
+    def test2_computeOutput(self):
+        """
+        Given a warn threshold < CPU load < crit threshold
+	should return the 'WARNING' Nagios plugin exit code
+        """
+        self._objUtility    = Utility.Utility()
+        self._objDebug      = Debug.Debug()
+        myPlugin    = CheckLocalCpu.CheckLocalCpu(
+            name        = 'CHECK LOCAL CPU',
+            objDebug    = self._objDebug,
+            objUtility  = self._objUtility
+            )
+
+        myPlugin._cpuData = {
+            'totalWithoutIdle': {'cpuPercent': critical - 1, 'cpuTime': 12.34}
+            }
+
+        myPlugin.computeOutput(warningThreshold=warning, criticalThreshold=critical)
+        self.assertEqual(myPlugin._exitCode, myPlugin._exitCodes['WARNING'])
 
         
 
