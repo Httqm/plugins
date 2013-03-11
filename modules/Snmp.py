8a3d0e4a1719e2247296bcd94043f1b0c21c666f
Matthieu Fournet <fournet.matthieu@gmail.com>
2013-03-12 00:54:11 +0100

    Select SNMP version with dictionary-based switch-case

--- modules/Snmp.py
+++ modules/Snmp.py
@@ -7,6 +7,16 @@
 # ==> .1.3.6.1.2.1.1.7.0 = INTEGER: 72
 
 
+
+# source : http://pysnmp.sourceforge.net/examples/current/v1arch/manager/getgen.html
+
+from pysnmp.carrier.asynsock.dispatch import AsynsockDispatcher
+from pysnmp.carrier.asynsock.dgram import udp, udp6
+from pyasn1.codec.ber import encoder, decoder
+from pysnmp.proto import api
+from time import time
+
+
 class Snmp(object):
 
     def __init__(self, community, version, host):
@@ -14,6 +24,20 @@ class Snmp(object):
 
         """
 
+        # Protocol version to use
+        #pMod = api.protoModules[api.protoVersion1]
+        #pMod = api.protoModules[api.protoVersion2c]
+
+        # {'a': 1, 'b': 2,}.get('a', 9)
+        # source : http://stackoverflow.com/questions/60208/replacements-for-switch-statement-in-python
+        #version = '2'
+        defaultSnmpVersion = api.protoModules[api.protoVersion2c]
+        pMod = {
+            '1'  : api.protoModules[api.protoVersion1],
+            '2c' : api.protoModules[api.protoVersion2c]
+            }.get(version, defaultSnmpVersion)
+
+        print pMod
         pass
 
 
