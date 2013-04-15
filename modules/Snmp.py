a9734a4c0d7a3b8b4240f47338631868cf90fefb
Matthieu Fournet <fournet.matthieu@gmail.com>
2013-04-15 23:07:53 +0200

    Improve unit tests

--- modules/Snmp.py
+++ modules/Snmp.py
@@ -10,17 +10,16 @@
 #
 # SNMP WALK :
 #
-# snmpwalk -On -c public -v 2c 192.168.1.101 1.3.6.1.2.1.1.9.1.4
+# snmpwalk -On -c public -v 2c 192.168.1.101 1.3.6.1.2.1.1.9.1.2
 #   ==> returns :
-#   .1.3.6.1.2.1.1.9.1.4.1 = Timeticks: (40) 0:00:00.40
-#   .1.3.6.1.2.1.1.9.1.4.2 = Timeticks: (40) 0:00:00.40
-#   .1.3.6.1.2.1.1.9.1.4.3 = Timeticks: (40) 0:00:00.40
-#   .1.3.6.1.2.1.1.9.1.4.4 = Timeticks: (40) 0:00:00.40
-#   .1.3.6.1.2.1.1.9.1.4.5 = Timeticks: (40) 0:00:00.40
-#   .1.3.6.1.2.1.1.9.1.4.6 = Timeticks: (40) 0:00:00.40
-#   .1.3.6.1.2.1.1.9.1.4.7 = Timeticks: (40) 0:00:00.40
-#   .1.3.6.1.2.1.1.9.1.4.8 = Timeticks: (40) 0:00:00.40
-
+#   .1.3.6.1.2.1.1.9.1.2.1 = OID: .1.3.6.1.6.3.10.3.1.1
+#   .1.3.6.1.2.1.1.9.1.2.2 = OID: .1.3.6.1.6.3.11.3.1.1
+#   .1.3.6.1.2.1.1.9.1.2.3 = OID: .1.3.6.1.6.3.15.2.1.1
+#   .1.3.6.1.2.1.1.9.1.2.4 = OID: .1.3.6.1.6.3.1
+#   .1.3.6.1.2.1.1.9.1.2.5 = OID: .1.3.6.1.2.1.49
+#   .1.3.6.1.2.1.1.9.1.2.6 = OID: .1.3.6.1.2.1.4
+#   .1.3.6.1.2.1.1.9.1.2.7 = OID: .1.3.6.1.2.1.50
+#   .1.3.6.1.2.1.1.9.1.2.8 = OID: .1.3.6.1.6.3.16.2.2.1
 
 # /usr/local/lib/python2.6/dist-packages/pysnmp/entity/rfc3413/oneliner/cmdgen.py
 from pysnmp.entity.rfc3413.oneliner import cmdgen
@@ -28,89 +27,98 @@ from pysnmp.entity.rfc3413.oneliner import cmdgen
 
 class Snmp(object):
 
-    def __init__(self, utility, host, community='public', version='2c', timeoutMilliseconds=1000):
-        self._utility = utility
+    def __init__(self, utility, debug, host, port=161, community='public', version='2c', timeoutMilliseconds=1000):
+        self._utility   = utility
+        self._debug     = debug
         self._host = host
+        self._port = port
         self._community = community
-        self._timeoutMilliseconds = timeoutMilliseconds
         # TODO : version is unused
+        self._timeoutMilliseconds = timeoutMilliseconds
         self._cmdGen = cmdgen.CommandGenerator()
 
 
     def get(self, OID):
-        """
+        try:
+            errorIndication, errorStatus, errorIndex, varBinds = self._cmdGen.getCmd(
+                cmdgen.CommunityData(self._community),
 
-        """
-        errorIndication, errorStatus, errorIndex, varBinds = self._cmdGen.getCmd(
-            cmdgen.CommunityData(self._community),
+                # /usr/local/lib/python2.6/dist-packages/pysnmp/entity/rfc3413/oneliner/target.py
+                cmdgen.UdpTransportTarget(
+                    transportAddr = (self._host, self._port),
+                    timeout = self._timeoutMilliseconds / 1000,
+                    retries = 3
+                    ),
+                OID
+                )
+                # TODO : test the timeout + retries
+        except Exception, e:    # this catches errors such as invalid IP
+            self._debug.show('GET - ERROR 1 : %s' % e.args[0])
+            return None
 
-            # /usr/local/lib/python2.6/dist-packages/pysnmp/entity/rfc3413/oneliner/target.py
-            cmdgen.UdpTransportTarget(
-                transportAddr = (self._host, 161),
-                timeout = self._timeoutMilliseconds / 1000,
-                retries = 3
-                ),
-            OID
-            )
-            # TODO : test the timeout + retries
 
-        # Check for errors and print out results
-        if errorIndication:
-            print 'ERROR'
-            print(errorIndication)
-            # TODO : fix this
+        if errorIndication: # Check for errors (such as timeouts) and print out results
+            self._debug.show('GET - ERROR 2 : %s' % errorIndication)
+            return None
         else:
             if errorStatus:
-                print('%s at %s' % (
-                    errorStatus.prettyPrint(),
-                    errorIndex and varBinds[int(errorIndex)-1] or '?'
+                self._debug.show('%s at %s' % (
+                        errorStatus.prettyPrint(),
+                        errorIndex and varBinds[int(errorIndex)-1] or '?'
+                        )
                     )
-                )
-                # TODO : fix this
+                # TODO : don't print, 'except'
             else:
                 for oid, value in varBinds:
                     try:
                         oidValue = value if self._utility.isNumber(value) else str(value)
                     except AttributeError:
+                        self._debug.show('GET - ERROR 3 : Invalid OID')
                         # When the specified OID doesn't exist, 'value' doesn't either
                         oidValue = None
-
         return oidValue
 
 
     def walk(self, OID):
-        """
-
-        """
         returnData = {}
-        errorIndication, errorStatus, errorIndex, varBindTable = self._cmdGen.nextCmd(
-            cmdgen.CommunityData(self._community),
-            cmdgen.UdpTransportTarget((self._host, 161)),
-            OID,
-            )
 
-        if errorIndication:
-            print(errorIndication)
-            # TODO : don't print, 'except'
+        try:
+            errorIndication, errorStatus, errorIndex, varBindTable = self._cmdGen.nextCmd(
+                cmdgen.CommunityData(self._community),
+                cmdgen.UdpTransportTarget((self._host, self._port)),
+                OID,
+                )
+        except Exception, e:    # this catches errors such as invalid IP
+            self._debug.show('WALK - ERROR 1 : %s' % e.args[0])
+            return None
+
+        if errorIndication:     # Check for errors (such as timeouts) and print out results
+            self._debug.show('WALK - ERROR 2 : %s' % errorIndication)
+            return None
         else:
             if errorStatus:
-                print('%s at %s' % (
-                    errorStatus.prettyPrint(),
-                    errorIndex and varBindTable[-1][int(errorIndex)-1] or '?'
+                self._debug.show('%s at %s' % (
+                        errorStatus.prettyPrint(),
+                        errorIndex and varBindTable[-1][int(errorIndex)-1] or '?'
+                        )
                     )
-                )
                 # TODO : don't print, 'except'
             else:
+                if not varBindTable:    # list is empty because on invalid OID
+                    self._debug.show('WALK - ERROR 3 : Invalid OID')
+                    return None
+
+#                self._debug.show(varBindTable)
+
                 for varBindTableRow in varBindTable:
                     for oid, value in varBindTableRow:
-#                        print('%s = %s' % (oid.prettyPrint(), value.prettyPrint()))
-
+#                        self._debug.show('%s = %s' % (oid.prettyPrint(), value.prettyPrint()))
                         oid     = str(oid)
                         value   = str(value)
                         try:
                             returnData[oid] = float(value) if self._utility.isNumber(value) else value
                         except AttributeError:
-                            # When the specified OID doesn't exist, 'value' doesn't either
-                            returnData[oid] = None
-#            print returnData
+                            # When the specified OID doesn't exist, 'value' doesn't either.
+                            return None
+#            self._debug.show(returnData)
         return returnData
--- testU/test_Snmp.py
+++ testU/test_Snmp.py
@@ -15,10 +15,22 @@ os.sys.path.insert(0,parentdir)
 
 from modules import Snmp
 from modules import Utility
-#from modules import Debug
+from modules import Debug
 
-myUtility = Utility.Utility()
+myUtility   = Utility.Utility()
+myDebug     = Debug.Debug()
 
+#myDebug.enable(True)
+myDebug.enable(False)
+
+# test variables
+testHostIp          = '192.168.1.101'
+testHostPort        = 161
+testHostCommunity   = 'public'
+testHostVersion     = '2c'
+unusedIpAddress     = '192.168.42.42'
+invalidIpAddress    = '123.456.789.876'
+invalidOid          = '2.3.4.5.6.7.8'
 
 class test_Snmp(unittest.TestCase):
 
@@ -27,35 +39,34 @@ class test_Snmp(unittest.TestCase):
         Given community, version, host, OID = '1.3.6.1.2.1.1.7.0'
         Should return 72
         """
-        mySnmp = Snmp.Snmp(myUtility, host='192.168.1.101', community='public', version='2c')
+        mySnmp = Snmp.Snmp(
+            myUtility,
+            myDebug,
+            host    = testHostIp,
+            port    = testHostPort,
+            community   = testHostCommunity,
+            version     = testHostVersion,
+            timeoutMilliseconds = 1000
+            )
         self.assertEqual(mySnmp.get('1.3.6.1.2.1.1.7.0'), 72)
 
-#TESTING
-#    def test2_get(self):
-#        """
-#        Given community, version, host, and a non-existing OID = '1.2.3.4'
-#        Should return None
-#        """
-#        mySnmp = Snmp.Snmp(myUtility, host='192.168.1.101', community='public', version='2c')
-#        self.assertEqual(mySnmp.get('1.2.3.4'), None)
 
-
-#    def test3_get(self):
-#        """
-#        Given community, version, OID = '1.3.6.1.2.1.1.7.0' and an existing IP address with no SNMPd listening
-#        Should return None
-#        """
-#        mySnmp = Snmp.Snmp(myUtility, host='10.20.30.40', community='public', version='2c')
-#        self.assertEqual(mySnmp.get('1.3.6.1.2.1.1.7.0'), 72)
-
-
-#    def test4_get(self):
-#        """
-#        Given community, version, OID = '1.3.6.1.2.1.1.7.0' and a non-existing IP address
-#        Should return None
-#        """
-#        mySnmp = Snmp.Snmp(myUtility, host='123.456.789.987', community='public', version='2c')
-#        self.assertEqual(mySnmp.get('1.3.6.1.2.1.1.7.0'), 72)
+    def test2_get(self):
+        """
+        Given community, version, host, and a non-existing OID
+        Should return None
+        """
+        myDebug.show('This test MUST end on an "Invalid OID" error.')
+        mySnmp = Snmp.Snmp(
+            myUtility,
+            myDebug,
+            host    = testHostIp,
+            port    = testHostPort,
+            community   = testHostCommunity,
+            version     = testHostVersion,
+            timeoutMilliseconds = 1000
+            )
+        self.assertEqual(mySnmp.get(invalidOid), None)
 
 
     def test1_walk(self):
@@ -65,7 +76,15 @@ class test_Snmp(unittest.TestCase):
         """
         import string
 
-        mySnmp = Snmp.Snmp(myUtility, host='192.168.1.101', community='public', version='2c')
+        mySnmp = Snmp.Snmp(
+            myUtility,
+            myDebug,
+            host    = testHostIp,
+            port    = testHostPort,
+            community   = testHostCommunity,
+            version     = testHostVersion,
+            timeoutMilliseconds = 1000
+            )
         result = mySnmp.walk('1.3.6.1.2.1.1.9.1.2')
         resultIsADict = True if isinstance(result, dict) else False
 
@@ -76,3 +95,87 @@ class test_Snmp(unittest.TestCase):
             except ValueError:
                 valuesAreOk = False
         self.assertTrue(resultIsADict and valuesAreOk)
+
+
+    def test2_walk(self):
+        """
+        Given community, version, OID = '1.3.6.1.2.1.1.9.1.2' and an invalid IP address
+        Should return None
+        """
+        myDebug.show('This test MUST end on a "Bad IPv4/UDP transport address" error.')
+        mySnmp = Snmp.Snmp(
+            myUtility,
+            myDebug,
+            host    = invalidIpAddress,
+            port    = testHostPort,
+            community   = testHostCommunity,
+            version     = testHostVersion,
+            timeoutMilliseconds = 1000
+            )
+        self.assertEqual(mySnmp.walk('1.3.6.1.2.1.1.9.1.2'), None)
+
+
+    def test4_walk(self):
+        """
+        Given host, community, version, and an invalid OID
+        Should return None
+        """
+        myDebug.show('This test MUST end on a "Invalid OID" error.')
+        mySnmp = Snmp.Snmp(
+            myUtility,
+            myDebug,
+            host    = testHostIp,
+            port    = testHostPort,
+            community   = testHostCommunity,
+            version     = testHostVersion,
+            timeoutMilliseconds = 1000
+            )
+        self.assertEqual(mySnmp.walk(invalidOid), None)
+
+
+
+
+
+
+# Other tests not working for current Python version
+
+    # 'skip' requires Python >= 2.7
+    # http://docs.python.org/2/library/unittest.html#unittest-skipping
+    # @unittest.skip("Don't want to wait for timeout : skipping this test !")
+#    def test2_get(self):
+#        """
+#        Given community, version, OID = '1.3.6.1', and an address not used on the LAN
+#        Should return None
+#        """
+#        mySnmp = Snmp.Snmp(myUtility, host=unusedIpAddress, community=testHostCommunity, version=testHostVersion, timeoutMilliseconds=1)
+#        self.assertEqual(mySnmp.get('1.3.6.1'), None)
+        #self.assertIsNone(mySnmp.get('1.3.6.1'))   # Requires Python >= 2.7
+        # http://docs.python.org/2/library/unittest.html#assert-methods
+
+
+
+
+    # 'skip' requires Python >= 2.7
+    # http://docs.python.org/2/library/unittest.html#unittest-skipping
+    # @unittest.skip("Don't want to wait for timeout : skipping this test !")
+#    def test4_get(self):
+#        """
+#        Given community, version, OID = '1.3.6.1.2.1.1.7.0' and a non-existing IP address
+#        Should return None
+#        """
+#        mySnmp = Snmp.Snmp(myUtility, host=invalidIpAddress, community=testHostCommunity, version=testHostVersion)
+#        self.assertEqual(mySnmp.get('1.3.6.1.2.1.1.7.0'), None)
+
+
+
+
+    # 'skip' requires Python >= 2.7
+    # http://docs.python.org/2/library/unittest.html#unittest-skipping
+    # @unittest.skip("Don't want to wait for timeout : skipping this test !")
+#    def test2_walk(self):
+#        """
+#        Given community, version, OID = '1.3.6.1.2.1.1.9.1.2' and an unused IP address
+#        Should return None
+#        """
+#        mySnmp = Snmp.Snmp(myUtility, host=unusedIpAddress, community=testHostCommunity, version=testHostVersion)
+#        self.assertEqual(mySnmp.walk('1.3.6.1.2.1.1.9.1.2'), None)
