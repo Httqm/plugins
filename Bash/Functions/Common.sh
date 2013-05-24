e10ba47ad26373b6b627b59458f055e25d5e1e28
Matthieu Fournet <fournet.matthieu@gmail.com>
2013-05-25 01:26:32 +0200

    Add the colorEcho function, start dev of convertToBytes and skeleton of unit testing

--- Bash/Functions/Common.sh
+++ Bash/Functions/Common.sh
@@ -6,10 +6,10 @@
 # AUTHOR :		Matthieu FOURNET (matthieu.fournet@orange.com)
 # LICENSE :		GPL - http://www.fsf.org/licenses/gpl.txt
 #
-# NOTES :		1. 
+# NOTES :		1. Start this script with the '-t' flag to launch unit tests
 #
 #
-# VERSION :		20130507
+# VERSION :		20130525
 #
 # TODO :
 ########################################## ##########################################################
@@ -28,6 +28,19 @@ export STATE_DEPENDENT=4
 ########################################## ##########################################################
 
 #---------------------
+# 
+# ARGUMENTS :
+#	arg1 (STRING) :	human readable file size
+#
+# RETURN : (INT) size in bytes
+#---------------------
+convertToBytes() {
+	# cleaning the input data
+	cleanInputString=$(echo $1 | tr ', ' '.')
+	echo "|"$cleanInputString"|"
+	}
+
+#---------------------
 # Show (or not) the perfdata, return the exit code and leave script
 # ARGUMENTS :
 #	arg1 (STRING) :	output message
@@ -44,3 +57,58 @@ exitPlugin() {
 	echo "$1|$2"	# i.e. : "outputMessage|outputPerfdata"
 	exit $exitCode
 	}
+
+
+
+
+# TODO : format this
+
+# Echo text messages in color.
+
+# $1 : color
+# $2 : message
+# $3 : bold 0|1
+
+# Examples :
+#colorEcho red "red, normal" 0
+#colorEcho red "red, bold" 1
+
+#colorEcho green "green, normal" 0
+#colorEcho green "green, bold" 1
+function colorEcho() {
+	bold=${3:-0}		# Defaults to "not bold", if not specified.
+
+	declare -A colors	# required to declare an associative array
+	
+	colors['red']="\e[$bold;31m"
+	colors['green']="\e[$bold;32m"
+	colors['yellow']="\e[$bold;33m"
+	colors['blue']="\e[$bold;34m"
+	colors['purple']="\e[$bold;35m"
+	colors['cyan']="\e[$bold;36m"
+	colors['white']="\e[$bold;37m"
+	colors['reset']='\e[0m'	# Reset text attributes to normal without clearing screen.
+
+#	echo "couleur(1) : $1"
+#	echo "couleur(2) : ${colors[$1]}"
+#	echo "bold : $bold"
+
+	echo -e "${colors[$1]}$2${colors['reset']}"
+	}
+
+
+
+
+
+
+
+########################################## ##########################################################
+# UNIT TESTS
+########################################## ##########################################################
+if [ "$1" == '-t' ]
+then
+
+	echo -n 'convertToBytes '
+	[ "$(convertToBytes ' 1.2M ')" == '|1.2M|' ] && colorEcho green OK || colorEcho red KO
+
+fi
