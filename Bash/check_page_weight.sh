e647c57162f926d32538825bb822c4ce36489f86
Matthieu Fournet <fournet.matthieu@gmail.com>
2013-06-13 23:25:51 +0200

    Download content from 'static' domains to get REAL FULL page weight

--- Bash/check_page_weight.sh
+++ Bash/check_page_weight.sh
@@ -4,19 +4,28 @@
 # FUNCTION :    Measure the weight of the given webpage, including its HTML source code and linked
 #               files (CSS, JS, images), and warn accordingly.
 #
-# AUTHOR :      Matthieu FOURNET (matthieu.fournet@orange.com)
+# AUTHOR :      Matthieu FOURNET (fournet.matthieu@gmail.com)
 # LICENSE :     GPL - http://www.fsf.org/licenses/gpl.txt
 #
-# NOTES :       1. All computation is made assuming 1KB = 1024 bytes (and so on for MB, GB, ...)
-#               2. This plugin outputs page weight in bytes, but external graphing tools may
+# NOTES :       1. Full page weight is determined by downloading the HTML source code of the
+#                  specified page ("-u" parameter), then downloading all linked objects (images, JS,
+#                  CSS). However, some websites host their linked content on a different domain,
+#                  i.e. the page 'http://www.example.com' may include images from
+#                  'http://static.example.com' and CSS from 'http://www2.example.com'. Content from
+#                  these domains won't be retrieved (with wget default options) as following links
+#                  carelessly may lead to downloading the whole Internets :-(
+#                  To get content from these domains, the "-d" parameter of this plugin instructs
+#                  wget to content content from the listed domains only.
+#               2. All computation is made assuming 1KB = 1024 bytes (and so on for MB, GB, ...)
+#               3. This plugin outputs page weight in bytes, but external graphing tools may
 #                  accommodate their Y-scale assuming 1KB = 1000 bytes, i.e. :
 #                  654321 B = 639 KB (1 KB = 1024 B), but may be graphed as 654 KB (1 KB = 1000 B).
 #
 # COMMAND LINE :
-#               check_page_weight.sh -u "<URL of page to check>" -w "<warn>" -c "<crit>"
-#               check_page_weight.sh -u "http://www.google.com" -w 50KB -c 100KB -v
+#               check_page_weight.sh -u "<URL of page to check>" -d "domains" -w "<warn>" -c "<crit>"
+#               check_page_weight.sh -u "http://www.example.com" -d "static.example.com,www2.example.com" -w 50KB -c 100KB -v
 #
-# VERSION :     20130605
+# VERSION :     20130613
 ########################################## ##########################################################
 
 absolutePathToCurrentFile=$(cd $(dirname "$0"); pwd)
@@ -38,14 +47,15 @@ exitShowHelp() {
         files (CSS, JS, images), and warn accordingly.
 
     USAGE:
-        $0 -u "<URL of page to check>" -w "<warn>" -c "<crit>"
+        $0 -u "<URL of page to check>" -d "domains" -w "<warn>" -c "<crit>"
 
     EXAMPLE:
-        $0 -u "http://www.google.fr" -w 50K -c 100K -v
+        $0 -u "http://www.example.com" -d "static.example.com,www2.example.com" -w 50KB -c 100KB -v
 
     OPTIONS:
         -h  Show this help
         -u  URL of page to check
+        -d  Comma-separated list of domains to download from
         -w  warning threshold
         -c  critical threshold
         -v  Verbose
@@ -58,13 +68,18 @@ GLOUBIBOULGA
 # Retrieve and get weight of a specified web page in human-readable format
 # ARGUMENTS :
 #   arg1 (STRING) : URL of page to get
+#   arg2 (STRING) : comma-separated list of domains to download from. Example : "static.example.com,www2.example.com"
 # RETURN    : void
 #---------------------
 function getPage() {
     tmpFileRoot="/tmp/pageWeight.$$"
     tmpFile1="$tmpFileRoot.1"
     tmpFile2="$tmpFileRoot.2"
-    wget -U "Internet Exploiter" -nv -np -p --delete-after "$1" &> $tmpFile1
+
+    [ -z "$2" ] && domainsOption='' || domainsOption="-H -D $2"
+
+    wget -U "Internet Exploiter" -nv -np -p $domainsOption --delete-after "$1" &> $tmpFile1
+
     tail -n 1 $tmpFile1 > $tmpFile2
 #    nbFiles=$(cut -d " " -f 2 $tmpFile2)
     weight=$(cut -d " " -f 4 $tmpFile2)
@@ -87,13 +102,16 @@ pluginPerfdata=''
 ########################################## ##########################################################
 # USER INPUT VALIDATION
 ########################################## ##########################################################
-while getopts 'h:u:w:c:v' option;do # http://man.cx/getopts
+while getopts 'h:u:d:w:c:v' option;do # http://man.cx/getopts
     case $option in
         h)
             exitShowHelp
             ;;
         u)
             OPT_URL=$OPTARG
+            ;;
+        d)
+            OPT_DOMAINS=$OPTARG
             ;;
         w)
             OPT_WARNING=$OPTARG
@@ -133,10 +151,11 @@ checkWarningIsLessThanCritical $warningThresholdBytes $criticalThresholdBytes
 # GETTING METRICS
 ########################################## ##########################################################
 
-humanReadablePageWeight=$(getPage $OPT_URL)
+humanReadablePageWeight=$(getPage "$OPT_URL" "$OPT_DOMAINS")
 pageWeightBytes=$(convertToBytes $humanReadablePageWeight)
 
 [ $OPT_VERBOSE -eq 1 ] && echo "Page weight = $pageWeightBytes B ($humanReadablePageWeight)"
+
 
 ########################################## ##########################################################
 # OUTPUT STATUS + PERFDATA
